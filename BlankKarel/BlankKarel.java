2610a6acf7aee1ea0cd42a5a47982a752f44e0ba
Artem Zolochevskiy <artem.zolochevskiy@gmail.com>
2014-07-27 20:42:19 +0300

    add BlankKarel project

---
+++ BlankKarel/BlankKarel.java
@@ -0,0 +1,20 @@
+/*
+ * File: BlankKarel.java
+ * ---------------------
+ * Blank Karel project.
+ */
+
+import stanford.karel.*;
+
+public class BlankKarel extends SuperKarel {
+
+	public void run() {
+		/* You fill this in. */
+	}
+
+	/* Standard Java entry point */
+	/* This method can be eliminated in most Java environments */
+	public static void main(String[] args) {
+		new BlankKarel().start(args);
+	}
+}
