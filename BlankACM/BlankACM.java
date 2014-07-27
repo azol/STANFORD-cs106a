b27cd9afbdf3d14ff803d180aa82a86577141a11
Artem Zolochevskiy <artem.zolochevskiy@gmail.com>
2014-07-27 20:46:03 +0300

    add BlankACM project

---
+++ BlankACM/BlankACM.java
@@ -0,0 +1,21 @@
+/*
+ * File: BlankACM.java
+ * -------------------
+ * Blank acm project.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class BlankACM extends GraphicsProgram {
+
+	public void run() {
+		/* You fill this in. */
+	}
+
+	/* Standard Java entry point */
+	/* This method can be eliminated in most Java environments */
+	public static void main(String[] args) {
+		new BlankACM().start(args);
+	}
+}
